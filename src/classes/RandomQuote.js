8fbca98649149a56e1d74f6e63ca208480e56844
Ivan-Odessa <the_beginning@ukr.net>
2025-02-27 13:28:01 +0200

    Removing comments for the async/await

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -1,7 +1,6 @@
 import quotes from '../data/quotes.js';
 import MathUtils from '../utils/MathUtils.js';
 import Quote from './Quote.js';
-
 class RandomQuote {
   static getRandomQuote() {
     const randomIndex = MathUtils.generateRandomInt(quotes.length);
@@ -9,44 +8,17 @@ class RandomQuote {
     return new Quote(id, text, author);
   }
 
-  /**
-   * 1. Each async function returns Promise
-   * 2.
-   * 3. Promise returned by the getRandomQuoteViaAPI function will be always "fulfilled"
-   * because we catch all possible errors
-   * 4. Result of the "fulfilled" promise will be either Quote or undefined
-   * 5. Therefore there is no need for try/catch block where we call this function
-   */
-
   static async getRandomQuoteViaAPI() {
     const url = 'https://dummyjson.com/quotes/random';
     const option = { headers: { 'content-Type': 'application/json' } };
     try {
       const response = await fetch(url, option);
       const { id, quote: text, author } = await response.json();
-      // resolves promise to Quote(promise becomes "fulfilled")
       return new Quote(id, text, author);
     } catch (error) {
       console.error(error);
-      /**
-       * 1. Returns undefined implicitly (resolves promise to undefined)
-       * 2. Promise becomes "fulfilled"
-       */
     }
   }
-
-  // static getRandomQuoteViaAPI() {
-  //   const url = 'https://dummyjson.com/quotes/random';
-  //   const option = { headers: { 'content-Type': 'application/json' } };
-
-  //   return fetch(url, option)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const { id, quote: text, author } = data;
-  //       return new Quote(id, text, author);
-  //     })
-  //     .catch((error) => console.log(error));
-  // }
 }
 
 export default RandomQuote;
