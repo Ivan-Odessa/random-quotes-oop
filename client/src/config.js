00fbda17f4424f100d59e7d6040cd4b718c4e44a
Ivan-Odessa <the_beginning@ukr.net>
2025-03-26 19:59:46 +0200

    Added randomQuote.js on server, change config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,10 @@
+const isLocalhost = window.location.hostname === 'localhost';
+
 const config = {
   PUBLIC_API_URL: 'https://dummyjson.com',
-  API_URL: 'https://random-quotes-oop.netlify.app',
+  API_URL: isLocalhost
+    ? 'http://localhost:3000'
+    : `${window.location.origin}/.netlify/functions/randomQuote`,
 };
 
 export default config;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const quotes = require('./data/quotes');
-const app = express();
-const PORT = 3000;
+if (process.env.NODE_ENV !== 'production') {
+  const express = require('express');
+  const cors = require('cors');
+  const quotes = require('./data/quotes');
+  const app = express();
+  const PORT = 3000;
 
-function getRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuotes = quotes[randomIndex];
-  return randomQuotes;
+  function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuotes = quotes[randomIndex];
+    return randomQuotes;
+  }
+  const corsOption = {
+    origin: '*', // default cross-origin value for CORS
+    // origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+  };
+
+  app.use(cors(corsOption));
+
+  app.get('/quotes/random-single', (req, res) => {
+    const randomQuote = getRandomQuote();
+    res.json(randomQuote);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Quotes API service is running on port ${PORT}`);
+  });
 }
-const corsOption = {
-  origin: '*', // default cross-origin value for CORS
-  // origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
-};
-
-app.use(cors(corsOption));
-
-app.get('/quotes/random-single', (req, res) => {
-  const randomQuote = getRandomQuote();
-  res.json(randomQuote);
-});
-
-app.listen(PORT, () => {
-  console.log(`Quotes API service is running on port ${PORT}`);
-});
