9a2bf347297ddc8906a112eb4e18d113a42498e8
Ivan-Odessa <the_beginning@ukr.net>
2026-05-19 17:02:38 +0300

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: 'a1c9f3d2',
+    text: 'It is never too late to be what you might have been',
+    author: 'George Eliot',
+  },
+  {
+    id: 'b7d2a91f',
+    text: 'We are what we repeatedly do. Excellence, then, is not an act, but a habit',
+    author: 'Aristotle',
+  },
+  {
+    id: 'c3f8e12a',
+    text: 'In the middle of difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    id: 'd9a4c6e1',
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'e2b7d8f5',
+    text: 'Do what you feel in your heart to be right, for you will be criticized anyway',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    id: 'f4c1a9d8',
+    text: 'Act as if what you do makes a difference. It does',
+    author: 'William James',
+  },
+  {
+    id: 'a8e3b6c7',
+    text: 'The best way out is always through',
+    author: 'Robert Frost',
+  },
+  {
+    id: 'b1f6d4a9',
+    text: 'You miss 100% of the shots you don’t take',
+    author: 'Wayne Gretzky',
+  },
+  {
+    id: 'c9d2e7b4',
+    text: 'Everything has beauty, but not everyone sees it',
+    author: 'Confucius',
+  },
+  {
+    id: 'd6a8f3c1',
+    text: 'He who is not courageous enough to take risks will accomplish nothing in life',
+    author: 'Muhammad Ali',
+  },
+];
+
+export default quotes;
