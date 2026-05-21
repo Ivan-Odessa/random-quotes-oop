e385d76d19903b9b84a281accfcb4d8b40db1a92
Ivan-Odessa <the_beginning@ukr.net>
2026-05-21 13:39:21 +0300

    Add  README.md and config in the client app

--- client/src/classes/RandomQuote.js
+++ client/src/classes/RandomQuote.js
@@ -1,6 +1,7 @@
 import quotes from '../data/quotes.js';
 import MathUtils from '../utils/MathUtils.js';
 import Quote from './Quote.js';
+import config from '../config.js';
 
 class RandomQuote {
   static getRandomQuote() {
@@ -10,7 +11,7 @@ class RandomQuote {
   }
 
   static async getRandomQuoteViaPublicApi() {
-    const url = 'https://dummyjson.com/quotes/random';
+    const url = `${config.PUBLIC_API_URL}/quotes/random`;
     const options = {
       headers: {
         'Content-Type': 'application/json',
@@ -28,7 +29,7 @@ class RandomQuote {
   }
 
   static async getRandomQuoteViaOwnApi() {
-    const url = 'http://localhost:3000/quotes/random-single';
+    const url = `${config.API_URL}/quotes/random-single`;
     const options = {
       headers: {
         'Content-Type': 'application/json',
---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  PUBLIC_API_URL: 'https://dummyjson.com',
+  API_URL: 'http://localhost:3000',
+};
+
+export default config;
